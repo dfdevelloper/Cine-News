a6a20c758196fa47b5ae6c415109bb98ef9f23b5
Danillo Fidel <danillopkt@hotmail.com>
2018-06-03 17:57:47 -0300

    responsivo

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -4,21 +4,35 @@
 
       switch (check) {
          case 1:
-            document.getElementById("logoHome").src = "_imagens/" + tipo + ".png";
+            document.getElementById("logoHome").src = "../_imagens/" + tipo + ".png";
             break;
          case 2:
-            document.getElementById("logoEmCartaz").src = "_imagens/" + tipo + ".png";
+            document.getElementById("logoEmCartaz").src = "../_imagens/" + tipo + ".png";
             break;
          case 3:
-            document.getElementById("logoSeries").src = "_imagens/" + tipo + ".png";
+            document.getElementById("logoSeries").src = "../_imagens/" + tipo + ".png";
          case 4:
-            document.getElementById("logoEmBreve").src = "_imagens/" + tipo + ".png";
+            document.getElementById("logoEmBreve").src = "../_imagens/" + tipo + ".png";
             break;
          case 5:
-            document.getElementById("logoRecomend").src = "_imagens/" + tipo + ".png";
+            document.getElementById("logoRecomend").src = "../_imagens/" + tipo + ".png";
          case 6:
-            document.getElementById("logoNews").src = "_imagens/" + tipo + ".png";
+            document.getElementById("logoNews").src = "../_imagens/" + tipo + ".png";
             break;
     }
 }
 
+    $(document).ready(function(){
+        
+        $("a").attr("target", "_self");
+        
+        var tam = $(window).width();
+        
+        if (tam < 800 ){
+            $("#corpo").removeAttr("style");
+            $("#corpo").css("marginTop", "-0.2%");
+            $("#menu").css("marginTop", "3%");
+        }
+        
+            
+    });
